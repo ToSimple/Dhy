90143abbd0448235e04bd52de4859ce20b324d71
ToSimple <403052342@qq.com>
2015-01-27 17:34:47 +0800

    上传控件测试，

---
+++ amakerMVC/Controllers/demoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace amakerMVC.Controllers
+{
+    public class demoController : Controller
+    {
+        //
+        // GET: /demo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        public ActionResult Save(FormCollection values)
+        {
+            string a = values["a"];
+            return Content("<img src=" + a + ">");
+        }
+
+        public ActionResult Save2(FormCollection values)
+        {
+            Jsondata j = new Jsondata();
+            j.Status = 1;
+            string a = values["base64"];
+            return Json(j,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
+public class Jsondata{
+    public int Status { get; set; }
+
+}
+
