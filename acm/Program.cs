33f5a78011850ac45f1bda1253981832152dec09
ToSimple <403052342@qq.com>
2015-03-11 17:37:35 +0800

    高精度乘法提交

--- acm/Program.cs
+++ acm/Program.cs
@@ -15,15 +15,51 @@ namespace acm
             //acm.Chen('8', '2');
             //string s = "99999999";
             //Console.WriteLine( acm.Chen(s.ToCharArray(), '9'));
-            string a = "9223372039223372039223372036854792233720392233720392233720375808.992233720392922337203922337203922337203922337203922337203233720392233720392233720392233720392233720322337203";
-            string b = "9223372092233720392233720336854922337203922337203922337203775808.922337203922337922337203922337203203922337203922337203922337203922337203922337203922337203922337203922337203";
-            char[] aa = acm.Sum(a.ToCharArray(), b.ToCharArray());
+            //string a = "18312312333720123123333922337203775808.992233720392922337203922337203922337203922337203922337203233720392233720392233720392233720392233720322337203";
+            //string b = "9223372092233720392233720336854922337203922337203922337203775808.31231233311";
+            //char[] aa = acm.Sum(a.ToCharArray(), b.ToCharArray());
 
-            for (int i = aa.Count()-1; i >=0; i--)
+            //for (int i = aa.Count()-1; i >=0; i--)
+            //{
+            //    Console.Write(aa[i]);
+            //}
+
+
+            //string a = "199";
+            //string b = "1";
+            //char[] aa = acm.SumF(a, b);
+            //for (int i = aa.Count() - 1; i >= 0; i--)
+            //{
+            //    Console.Write(aa[i]);
+            //}
+
+            // string a = "123";
+            //string b= "1233";
+            //string a = "123";
+            //string b = "11.123";
+            //string b = "4";
+            // string a = "3.1231234413";
+            //char[] aa = acm.Diff(a, b);
+
+
+            //string a = "43958234589324598345.534523450823094580349859034289508";
+            //int b = 9;
+            //char[] aa = acm.QuadUnit(a,b);
+
+            //for (int i = aa.Count() - 1; i >= 0; i--)
+            //{
+            //    Console.Write(aa[i]);
+            //}
+            string a = "812389.172894";
+            string b = "560.12314441";
+            char[] aa = acm.Quadrature(a, b);
+
+            foreach (var itam in aa)
             {
-                Console.Write(aa[i]);
+                Console.Write(itam);
             }
-           
-        } 
+
+
+        }
     }
 }
--- acm/acm.cs
+++ acm/acm.cs
@@ -181,12 +181,6 @@ namespace acm
             return re;
         }
 
-
-
-
-
-
-
         public static char[] Sum(string a, string b)
         {
             return Sum(a.ToCharArray(), b.ToCharArray());
@@ -320,11 +314,23 @@ namespace acm
                 }
                 else if (cl <= 0 && dl > 0)
                 {
-                    r = d[dl - 1] - 48;
+                    r = d[dl - 1] - 48 + jw;
+                    jw = 0;
+                    if (r >= 10)
+                    {
+                        jw++;
+                        r = r - 10;
+                    }
                 }
                 else if (cl > 0 && dl <= 0)
                 {
-                    r = c[cl - 1] - 48;
+                    r = c[cl - 1] - 48 + jw;
+                    jw = 0;
+                    if (r >= 10)
+                    {
+                        jw++;
+                        r = r - 10;
+                    }
                 }
 
                 if (r == 46)
@@ -346,7 +352,12 @@ namespace acm
 
         public static void Buwei(char[] a, char[] b)
         {
-            int alength = a.Count();
+            char[] c = a;//补位后的a
+            char[] d = b;//补位后的b
+            int alength = a.Count();//a长度
+            int blength = b.Count();//b长度
+            int adot = alength;//a小数点位置
+            int bdot = blength;//b小数点位置
 
             for (int i = 0; i < alength; i++)
             {
@@ -364,8 +375,539 @@ namespace acm
                     break;
                 }
             }
+            if (adot != alength || bdot != blength)
+            {
+                if (adot == alength)
+                {
+                    c = new char[alength + 1 + blength - bdot - 1];
+                    for (int i = 0; i < c.Length; i++)
+                    {
+                        if (i < alength)
+                        {
+                            c[i] = a[i];
+                        }
+                        else if (i == alength)
+                        {
+                            c[i] = '.';
+                        }
+                        else
+                        {
+                            c[i] = '0';
+                        }
+                    }
+
+                }
+                else if (bdot == blength)
+                {
+                    d = new char[blength + 1 + alength - adot - 1];
+                    for (int i = 0; i < d.Length; i++)
+                    {
+                        if (i < blength)
+                        {
+                            d[i] = b[i];
+                        }
+                        else if (i == blength)
+                        {
+                            d[i] = '.';
+                        }
+                        else
+                        {
+                            d[i] = '0';
+                        }
+                    }
+                }
+                else
+                {
+                    if (alength - adot > blength - bdot)
+                    {
+                        d = new char[blength + ((alength - adot) - (blength - bdot))];
+                        for (int i = 0; i < d.Length; i++)
+                        {
+                            if (i < blength)
+                            {
+                                d[i] = b[i];
+                            }
+                            else
+                            {
+                                d[i] = '0';
+                            }
+                        }
+                    }
+                    else
+                    {
+                        c = new char[alength + ((blength - bdot) - (alength - adot))];
+                        for (int i = 0; i < c.Length; i++)
+                        {
+                            if (i < alength)
+                            {
+                                c[i] = a[i];
+                            }
+                            else
+                            {
+                                c[i] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+            a = c;
+            b = d;
+            //foreach (var item in a)
+            //{
+            //    Console.Write(item);
+            //}
+            //Console.WriteLine();
+            //foreach (var item in b)
+            //{
+            //    Console.Write(item);
+            //}
+
         }
 
+        public static void BuweiF(ref string a, ref string b)
+        {
+            int al = a.Length;
+            int bl = b.Length;
+            string[] c = a.Split('.');
+            string[] d = b.Split('.');
+            int i = al - c[0].Length;
+            int j = bl - d[0].Length;
+            if (i != 0 && j != 0)
+            {
+                if (i > j)
+                {
+                    while (i != j)
+                    {
+                        b += "0";
+                        j++;
+                    }
+                }
+                else
+                {
+                    while (i != j)
+                    {
+                        a += "0";
+                        i++;
+                    }
+                }
+            }
+            else if (i != 0 && j == 0)
+            {
+                b += ".";
+                j++;
+                while (i != j)
+                {
+                    b += "0";
+                    j++;
+                }
+            }
+            else if (i == 0 && j != 0)
+            {
+                a += ".";
+                i++;
+                while (i != j)
+                {
+                    a += "0";
+                    i++;
+                }
+            }
+            //Console.WriteLine(a);
+            // Console.WriteLine(b);
+        }
 
+        public static char[] SumF(string a, string b)
+        {
+            BuweiF(ref a, ref b);//小数点后补位
+            List<char> item = new List<char>();
+            List<char> ritems = new List<char>();
+            int al = a.Length;
+            int bl = b.Length;
+            int r = 0;
+            int jw = 0;//进位
+            do
+            {
+                if (al > 0 && bl > 0)
+                {
+                    if (a[al - 1] == '.')
+                    {
+                        r = '.';
+                    }
+                    else
+                    {
+                        r = (int)a[al - 1] + (int)b[bl - 1] - 96 + jw;
+                        jw = 0;
+                        if (r >= 10)
+                        {
+                            jw++;
+                            r = r - 10;
+                        }
+                    }
+                }
+                else if (al <= 0 && bl > 0)
+                {
+                    r = b[bl - 1] - 48 + jw;
+                    jw = 0;
+                    if (r >= 10)
+                    {
+                        jw++;
+                        r = r - 10;
+                    }
+                }
+                else if (al > 0 && bl <= 0)
+                {
+                    r = a[al - 1] - 48 + jw;
+                    jw = 0;
+                    if (r >= 10)
+                    {
+                        jw++;
+                        r = r - 10;
+                    }
+                }
+
+                if (r == 46)
+                {
+                    item.Add(Convert.ToChar(r));
+                }
+                else
+                {
+                    item.Add(Convert.ToChar(r.ToString()));
+                }
+                al--; bl--;
+            } while (al > 0 || bl > 0);
+            if (jw == 1)
+            {
+                item.Add('1');
+            }
+            for (int i = item.Count() - 1; i >= 0; i--)
+            {
+                ritems.Add(item[i]);
+            }
+            return ritems.ToArray();
+        }
+
+        public static char[] Diff(string a, string b)
+        {
+            BuweiF(ref a, ref b);
+            List<char> item = new List<char>();
+            int al = a.Length;
+            int bl = b.Length;
+            int r = 0;
+            int jw = 0;//退位
+            bool fushu = false;
+            if (al > bl)
+            {
+                do
+                {
+                    if (al > 0 && bl > 0)
+                    {
+                        if (a[al - 1] == '.')
+                        {
+                            r = '.';
+                        }
+                        else
+                        {
+                            r = (int)a[al - 1] - (int)b[bl - 1] - jw;
+                            jw = 0;
+                            if (r < 0)
+                            {
+                                r = 10 + r;
+                                jw++;
+                            }
+                        }
+                    }
+                    else if (al > 0 && bl <= 0)
+                    {
+                        r = a[al - 1] - 48 - jw;
+                        jw = 0;
+                        if (r < 0)
+                        {
+                            r = 10 + r;
+                            jw++;
+                        }
+                    }
+
+                    if (r == 46)
+                    {
+                        item.Add(Convert.ToChar(r));
+                    }
+                    else
+                    {
+                        item.Add(Convert.ToChar(r.ToString()));
+                    }
+                    al--; bl--;
+                } while (al > 0 || bl > 0);
+            }
+            else if (al < bl)
+            {
+                fushu = true;
+                do
+                {
+                    if (al > 0 && bl > 0)
+                    {
+                        if (b[bl - 1] == '.')
+                        {
+                            r = '.';
+                        }
+                        else
+                        {
+                            r = (int)b[bl - 1] - (int)a[al - 1] - jw;
+                            jw = 0;
+                            if (r < 0)
+                            {
+                                r = 10 + r;
+                                jw++;
+                            }
+                        }
+                    }
+                    else if (bl > 0 && al <= 0)
+                    {
+                        r = b[bl - 1] - 48 - jw;
+                        jw = 0;
+                        if (r < 0)
+                        {
+                            r = 10 + r;
+                            jw++;
+                        }
+                    }
+
+                    if (r == 46)
+                    {
+                        item.Add(Convert.ToChar(r));
+                    }
+                    else
+                    {
+                        item.Add(Convert.ToChar(r.ToString()));
+                    }
+                    al--; bl--;
+                } while (al > 0 || bl > 0);
+            }
+            else
+            {
+                if (a[0] > b[0])
+                {
+                    do
+                    {
+                        if (al > 0 && bl > 0)
+                        {
+                            if (a[al - 1] == '.')
+                            {
+                                r = '.';
+                            }
+                            else
+                            {
+                                r = (int)a[al - 1] - (int)b[bl - 1] - jw;
+                                jw = 0;
+                                if (r < 0)
+                                {
+                                    r = 10 + r;
+                                    jw++;
+                                }
+                            }
+                        }
+                        else if (al > 0 && bl <= 0)
+                        {
+                            r = a[al - 1] - 48 - jw;
+                            jw = 0;
+                            if (r < 0)
+                            {
+                                r = 10 + r;
+                                jw++;
+                            }
+                        }
+
+                        if (r == 46)
+                        {
+                            item.Add(Convert.ToChar(r));
+                        }
+                        else
+                        {
+                            item.Add(Convert.ToChar(r.ToString()));
+                        }
+                        al--; bl--;
+                    } while (al > 0 || bl > 0);
+                }
+                else
+                {
+                    fushu = true;
+                    do
+                    {
+                        if (al > 0 && bl > 0)
+                        {
+                            if (b[bl - 1] == '.')
+                            {
+                                r = '.';
+                            }
+                            else
+                            {
+                                r = (int)b[bl - 1] - (int)a[al - 1] - jw;
+                                jw = 0;
+                                if (r < 0)
+                                {
+                                    r = 10 + r;
+                                    jw++;
+                                }
+                            }
+                        }
+                        else if (bl > 0 && al <= 0)
+                        {
+                            r = b[bl - 1] - 48 - jw;
+                            jw = 0;
+                            if (r < 0)
+                            {
+                                r = 10 + r;
+                                jw++;
+                            }
+                        }
+
+                        if (r == 46)
+                        {
+                            item.Add(Convert.ToChar(r));
+                        }
+                        else
+                        {
+                            item.Add(Convert.ToChar(r.ToString()));
+                        }
+                        al--; bl--;
+                    } while (al > 0 || bl > 0);
+                }
+            }
+            if (item[item.Count() - 1] == '0' && item[item.Count() - 2] != '.')
+            {
+                item.RemoveAt(item.Count() - 1);
+            }
+            if (fushu)
+            {
+                item.Add('-');
+            }
+            List<char> ritems = new List<char>();
+
+            for (int i = item.Count() - 1; i >= 0; i--)
+            {
+                ritems.Add(item[i]);
+
+            }
+            return ritems.ToArray();
+
+        }
+
+        public static char[] Quadrature(string a, string b)
+        {
+            char[] r = new char[1] { '0' };
+            List<int> bu = new List<int>();
+            List<int> au = new List<int>();
+            int bdot = 0;
+            int adot = 0;
+            for (int i = b.Length - 1; i >= 0; i--)
+            {
+                if (b[i] == '.')
+                {
+                    bdot = i;
+                }
+                else
+                {
+                    bu.Add(b[i] - 48);
+                }
+            }
+            for (int i =a.Length - 1; i >= 0; i--)
+            {
+                if (a[i] == '.')
+                {
+                    adot = i;
+                }
+                else
+                {
+                    au.Add(a[i] - 48);
+                }
+            }
+            a="";
+            for (int i = au.Count() - 1; i >= 0; i--)
+            {
+                a += au[i];
+            }
+
+            for (int i = 0; i < bu.Count(); i++)
+            {
+                char[] qq = QuadUnit(a, bu[i]);
+                string qqq = "";
+                for (int j = 1; j < i + 1; j++)
+                {
+                    qqq = "";            
+                    foreach (var item in qq)
+                    {
+                        qqq += item;
+                    }
+                    qq = QuadUnit(qqq, 10);
+                } 
+                qqq = "";
+                foreach (var item in qq)
+                {
+                    qqq += item;
+                }
+                string rr = "";
+                foreach (var item in r)
+                {
+                    rr += item;
+                }
+                r= SumF(rr, qqq);
+
+            }
+            char[] re =new char[r.Length+1];
+            int jjj = 0;
+            for (int i = 0; i < re.Length; i++)
+            {
+                if (i ==re.Length-(a.Length - adot + b.Length - bdot))
+                {
+                    if (i != a.Length + b.Length)
+                    {
+                        re[i] = '.';
+                    }                   
+                }
+                else
+                {
+                    re[i] += r[jjj];
+                    jjj++;
+                }
+               
+            }
+            return re;
+        }
+        public static char[] QuadUnit(string a, int b)
+        {
+            int r = 0;
+            int jw = 0;
+            char[] c = a.ToCharArray();
+            List<char> items = new List<char>();
+            List<char> ritems = new List<char>();
+            for (int i = c.Count() - 1; i >= 0; i--)
+            {
+                if (a[i] == '.')
+                {
+                    r = '.';
+                    items.Add(Convert.ToChar(r));
+                }
+                else
+                {
+                    r = ((int)c[i] - 48) * b + jw;
+                    jw = 0;
+                    if (r >= 10)
+                    {
+                        jw = r / 10;
+                        r %= 10;
+                    }
+                    items.Add(Convert.ToChar(r.ToString()));
+                }
+
+            }
+            if (jw > 0)
+            {
+                items.Add(Convert.ToChar(jw.ToString()));
+            }
+ 
+            for (int i = (items.Count() - 1); i >= 0; i--)
+            {                
+                ritems.Add(items[i]);
+            }
+            return ritems.ToArray();
+        }
     }
 }
