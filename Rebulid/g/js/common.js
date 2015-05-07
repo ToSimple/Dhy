30ff70fac9bbfacfbe5dc709086849f33ee2c807
ToSimple <403052342@qq.com>
2015-05-07 17:26:51 +0800

    日常

--- Rebulid/g/js/common.js
+++ Rebulid/g/js/common.js
@@ -12,26 +12,37 @@ function clearSay() {
 }
 
 //收缩
-$(".tip-min").on("click", function () {
+//$(".tip-min").on("click", function () {
+//    if ($(this).parent().parent().parent().children(".box-content-pd").css("display") == "none") {
 
-
-
-    if ($(this).parent().parent().parent().children(".box-content-pd").css("display") == "none") {
-
-        $(this).children("i").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
-        $(this).parent().parent().parent().children(".box-content-pd").css("display", "block");
-    } else if ($(this).parent().parent().parent().children(".box-content-pd").css("display") == "block") {
-
+//        $(this).children("i").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
+//        $(this).parent().parent().parent().children(".box-content-pd").css("display", "block");
+//    } else if ($(this).parent().parent().parent().children(".box-content-pd").css("display") == "block") {
+//        $(this).children("i").removeClass("fa-caret-square-o-down").addClass("fa-caret-square-o-up");
+//        $(this).parent().parent().parent().children(".box-content-pd").css("display", "none");
+//    }
+//    if ($(this).parent().parent().parent().children(".box-content").css("display") == "none") {
+//        $(this).children("i").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
+//        $(this).parent().parent().parent().children(".box-content").css("display", "block");
+//    } else if ($(this).parent().parent().parent().children(".box-content").css("display") == "block") {
+//        $(this).children("i").removeClass("fa-caret-square-o-down").addClass("fa-caret-square-o-up");
+//        $(this).parent().parent().parent().children(".box-content").css("display", "none");
+//    }
+//});
+$(".box-head").on("click", function () {
+    if ($(this).parent().children(".box-content-pd").css("display") == "none") {
+        $(this).children().children().children("i").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
+        $(this).parent().children(".box-content-pd").css("display", "block");
+    } else if ($(this).parent().children(".box-content-pd").css("display") == "block") {
         $(this).children("i").removeClass("fa-caret-square-o-down").addClass("fa-caret-square-o-up");
-        $(this).parent().parent().parent().children(".box-content-pd").css("display", "none");
+        $(this).parent().children(".box-content-pd").css("display", "none");
     }
-    if ($(this).parent().parent().parent().children(".box-content").css("display") == "none") {
 
-        $(this).children("i").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
-        $(this).parent().parent().parent().children(".box-content").css("display", "block");
-    } else if ($(this).parent().parent().parent().children(".box-content").css("display") == "block") {
-
-        $(this).children("i").removeClass("fa-caret-square-o-down").addClass("fa-caret-square-o-up");
-        $(this).parent().parent().parent().children(".box-content").css("display", "none");
+    if ($(this).parent().children(".box-content").css("display") == "none") {
+        $(this).children().children().children("i").removeClass("fa-caret-square-o-up").addClass("fa-caret-square-o-down");
+        $(this).parent().children(".box-content").css("display", "block");
+    } else if ($(this).parent().children(".box-content").css("display") == "block") {
+        $(this).children().children().children("i").removeClass("fa-caret-square-o-down").addClass("fa-caret-square-o-up");
+        $(this).parent().children(".box-content").css("display", "none");
     }
-})
+});
