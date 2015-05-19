0ece5ac1417fa7b911d8cb9306e723bb747ed3c4
ToSimple <403052342@qq.com>
2015-05-19 17:26:03 +0800

    1

--- Rebulid/g/js/taskCreSec.js
+++ Rebulid/g/js/taskCreSec.js
@@ -1,6 +1,6 @@
 ﻿
-$(function () {
 
+$(function () {
     $(".btn").on("click", function () {
         var a = $("#a").val();
         var b = $("#b").val();
@@ -118,7 +118,7 @@ $(function () {
             $(".dia-pan-con").html(str);
         }
     }
-    //组装未选择的标签 在加载列表开头 seach用到 
+    //组装未选择的标签 在加载列表开头 搜索用到 
     function dialogpanNoChecked(results) {
         //results有值，生成选中对象标签
         if (results != undefined) {
@@ -181,9 +181,9 @@ $(function () {
             }
         });
 
-        
+
         if (str.lastIndexOf(",") == (str.length - 1)) {
-            str = str.substr(0, str.length-1);     
+            str = str.substr(0, str.length - 1);
         }
         $("#pan").val(str);
         if (!pd) {
@@ -195,7 +195,7 @@ $(function () {
         }
 
     });
-
+    //搜索
     $("#searchper").on("change", function () {
         var formData = {
             'name': $(this).val()
@@ -210,4 +210,136 @@ $(function () {
             },
         });
     });
+
+    //团队
+    var two = true;//锁
+    //绑定事件
+    $("#obj2").on("click", function () {
+        if (two) {
+            //查看隐藏域是否有数据
+            var hid = $("#tuan").val();
+            //如果有数据 ，获取数据填写表单
+            if (hid != "") {
+                var formData = {
+                    'userIds': hid
+                };
+                $.ajax({
+                    url: "taskCreSec.aspx",
+                    data: formData,
+                    type: "get",
+                    dataType: "json",
+                    beforeSend: function () {
+                        $(".dg-load").css("display", "block");
+                        two = false;
+                    },
+                    success: function (results) {
+                        $(".dg-load").css("display", "none")
+                        dialogTuan(results);
+                        showOrClosetuan();
+                    },
+                    complete: function () {
+                        $(".dg-load").css("display", "none");
+                        two = true;
+                    }
+                });
+            } else {
+                //隐藏域为空，加载空表单
+                showOrClosetuan();
+            }
+
+        }
+    });
+    //组装选中checked的标签 初始加载用到
+    function dialogTuan(results) {
+        //results有值，生成选中对象标签
+        if (results != undefined) {
+            var str = "";
+            $.each(results.users, function () {
+                str += '<div class="dia-conItem dia-tuan">';
+                str += '<input type="checkbox" data-role="none" value="' + this.id + '" checked="checked" >' + this.name;
+                str += '</div>';
+            });
+            $(".dia-tuan-con").html(str);
+        }
+    }
+    //组装未选择的标签 在加载列表开头 搜索用到 
+    function dialogTuanNoChecked(results) {
+        //results有值，生成选中对象标签
+        if (results != undefined) {
+            var str = "";
+            $.each(results.users, function () {
+                str += '<div class="dia-conItem dia-tuan">';
+                str += '<input type="checkbox" data-role="none" value="' + this.id + '" >' + this.name;
+                str += '</div>';
+            });
+            $(".dia-tuan-con").prepend(str);
+        }
+    }
+    //显示还是隐藏模拟窗口
+    function showOrClosetuan() {
+        if ($("#dg-tuan").css("display") == "none") {
+            $("#searchper").val("");
+            $("#dg-tuan").css("display", "block");
+        } else {
+            $("#dg-tuan").css("display", "none");
+        }
+    }
+    //关闭按钮
+    $("#close-tuan").on("click", function () {
+       var eq= $("#tuan").val(str);
+       if (eq!="") {
+            $("#obj2").prop("checked", false);
+            $("#obj2").parent().children().first().removeClass("ui-checkbox-on").addClass("ui-checkbox-off");
+        } else {
+            $("#obj2").prop("checked", true);
+            $("#obj2").parent().children().first().removeClass("ui-checkbox-off").addClass("ui-checkbox-on");
+        }
+        showOrClosetuan();
+    });
+    //选定按钮
+    $("#xd-tuan").on("click", function () {
+        showOrClosetuan();
+        var items = $(".dia-tuan > input:checkbox");
+        var pd = false;
+        var str = "";
+        //遍历每个对象，如果未选中则删除dom标签
+        $.each(items, function () {
+            if (this.checked) {
+                str += this.value + ",";
+                pd = true;
+            } else {
+                this.parentNode.parentNode.removeChild(this.parentNode);
+            }
+        });
+
+
+        if (str.lastIndexOf(",") == (str.length - 1)) {
+            str = str.substr(0, str.length - 1);
+        }
+        $("#tuan").val(str);
+        if (!pd) {
+            $("#obj2").prop("checked", false);
+            $("#obj2").parent().children().first().removeClass("ui-checkbox-on").addClass("ui-checkbox-off");
+        } else {
+            $("#obj2").prop("checked", true);
+            $("#obj2").parent().children().first().removeClass("ui-checkbox-off").addClass("ui-checkbox-on");
+        }
+
+    });
+    //搜索
+    $("#searchTuan").on("change", function () {
+        var formData = {
+            'name': $(this).val()
+        };
+        $.ajax({
+            url: "taskCreSec.aspx",
+            data: formData,
+            type: "get",
+            dataType: "json",
+            success: function (results) {
+                dialogTuanNoChecked(results);
+            },
+        });
+    });
+
 })
