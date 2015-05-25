7c2abe05e557894c32e57615978b13431e9913d1
ToSimple <403052342@qq.com>
2015-05-25 17:31:01 +0800

    新首页

---
+++ Rebulid/g/index/proList.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="proList.aspx.cs" Inherits="g_index_proList" %>
---
+++ Rebulid/g/index/proList.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class g_index_proList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        System.Threading.Thread.Sleep(500);
+        string callback = Request.QueryString["jsoncallback"];
+        string data = "{\"list\":[{\"id\":\"1\",\"img\":\"../img/default.jpg\",\"title\":\"创客的项目\"},{\"id\":\"2\",\"img\":\"../img/default.jpg\",\"title\":\"创客的项目\"},{\"id\":\"3\",\"img\":\"../img/default.jpg\",\"title\":\"创客的项目\"},{\"id\":\"4\",\"img\":\"../img/default.jpg\",\"title\":\"创客的项目\"},{\"id\":\"5\",\"img\":\"../img/default.jpg\",\"title\":\"创客的项目\"},{\"id\":\"6\",\"img\":\"../img/default.jpg\",\"title\":\"创客的项目\"},{\"id\":\"7\",\"img\":\"../img/default.jpg\",\"title\":\"创客的项目\"}] }";
+        string result = string.Format("{0}{1}", callback, data);
+        Response.Expires = -1;
+        Response.Clear();
+        Response.ContentEncoding = Encoding.UTF8;
+        Response.ContentType = "application/json";
+        Response.Write(result);
+        Response.Flush();
+        Response.End();
+    }
+}
